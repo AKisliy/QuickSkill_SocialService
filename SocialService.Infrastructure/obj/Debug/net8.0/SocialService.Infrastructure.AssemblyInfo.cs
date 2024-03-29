0bff78e4e4adb68a437e0e4f6efe0a3eeac11c54
AKisliy <lekha.a-kisel@yandex.ru>
2024-03-29 14:48:06 +0300

    Change architecture a little, add shared library for better structure

--- SocialService.Infrastructure/obj/Debug/net8.0/SocialService.Infrastructure.AssemblyInfo.cs
+++ SocialService.Infrastructure/obj/Debug/net8.0/SocialService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0559929ca704aef4d8898969827ef767dbb4824f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6869699c8da99257579259564b4b5c422bfe4f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
